4329666f74c32806501cb1c4a329046f121411ea
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-03-30 22:35:44 +0000

    Set content type and write raw response in exception-handling controller This looks like it’s a change after upgrading to Spring 5

--- src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
+++ src/main/java/uk/ac/ebi/atlas/controllers/JsonExceptionHandlingController.java
@@ -5,34 +5,51 @@ import com.google.gson.JsonObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 import static org.apache.commons.lang3.StringUtils.isBlank;
 import static uk.ac.ebi.atlas.utils.GsonProvider.GSON;
 
-//TODO Make all controllers that produce JSON data inherit from this class
 public abstract class JsonExceptionHandlingController {
     private static final Logger LOGGER = LoggerFactory.getLogger(JsonExceptionHandlingController.class);
 
     @ExceptionHandler(BioentityNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    @ResponseBody
-    public String handleException(BioentityNotFoundException e) {
-        LOGGER.error("{} - {}", e.getMessage(), Joiner.on("\n\t").join(e.getStackTrace()));
-        return GSON.toJson(jsonError(isBlank(e.getMessage()) ? "Unknown error" : e.getMessage()));
+    public void handleException(BioentityNotFoundException e, HttpServletResponse response) {
+        writeJsonErrorMessage(e, response);
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-    @ResponseBody
-    public String handleException(Exception e) {
-        LOGGER.error("{} - {}", e.getMessage(), Joiner.on("\n\t").join(e.getStackTrace()));
-        return GSON.toJson(jsonError(isBlank(e.getMessage()) ? "Unknown error" : e.getMessage()));
+    public void handleException(Exception e, HttpServletResponse response) {
+        writeJsonErrorMessage(e, response);
     }
 
-    private JsonObject jsonError(String message) {
+    private static void writeJsonErrorMessage(Exception originatingException, HttpServletResponse response) {
+        LOGGER.error(
+                "{} - {}",
+                originatingException.getMessage(),
+                Joiner.on("\n\t").join(originatingException.getStackTrace()));
+        response.setHeader("content-type", MediaType.APPLICATION_JSON_UTF8_VALUE);
+        try {
+            response.getWriter().println(
+                    GSON.toJson(
+                            jsonError(isBlank(originatingException.getMessage()) ?
+                                    "Unknown error" :
+                                    originatingException.getMessage())));
+        } catch (IOException _e) {
+            throw new UncheckedIOException(_e);
+        }
+    }
+
+    private static JsonObject jsonError(String message) {
         JsonObject result = new JsonObject();
         result.addProperty("error", message);
         return result;
