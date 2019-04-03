d42a6e9095e349c824d4f183ec4af726987bb87b
alfonsomunozpomer <amunoz@ebi.ac.uk>
2019-04-03 02:20:06 +0100

    Check if both experiment and array designs are present in ArrayExpress, skipping creating the link if they’re not

--- src/main/java/uk/ac/ebi/atlas/experimentpage/LinkToArrayExpress.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/LinkToArrayExpress.java
@@ -1,30 +1,50 @@
 package uk.ac.ebi.atlas.experimentpage;
 
-import com.google.common.collect.ImmutableList;
+import org.apache.commons.lang3.tuple.Pair;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.util.UriBuilder;
 import uk.ac.ebi.atlas.model.download.ExternallyAvailableContent;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.model.experiment.differential.DifferentialExperiment;
 import uk.ac.ebi.atlas.model.experiment.differential.microarray.MicroarrayExperiment;
 
-import javax.inject.Named;
+import java.net.URI;
 import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static com.google.common.collect.ImmutableList.toImmutableList;
-import static java.util.Collections.singleton;
 
 public abstract class LinkToArrayExpress<E extends Experiment> extends ExternallyAvailableContent.Supplier<E> {
+    // You’ll get a 302 if the last slash is missing!
+    private static final UriBuilder EXPERIMENTS_URI_BUILDER =
+            new DefaultUriBuilderFactory().builder()
+                    .scheme("https")
+                    .host("www.ebi.ac.uk")
+                    .pathSegment("arrayexpress")
+                    .pathSegment("experiments")
+                    .pathSegment("{0}")
+                    .path("/");
+    private static final UriBuilder ARRAYS_URI_BUILDER =
+            new DefaultUriBuilderFactory().builder()
+                    .scheme("https")
+                    .host("www.ebi.ac.uk")
+                    .pathSegment("arrayexpress")
+                    .pathSegment("arrays")
+                    .pathSegment("{0}")
+                    .path("/");
+    private static final WebClient webClient = WebClient.create();
+    private static final WebClient arrayExpressWebClient = WebClient.create();
+
     private static final Function<Experiment, String> formatLabelToExperiment =
             e -> MessageFormat.format("ArrayExpress: experiment {0}", e.getAccession());
     private static final Function<String, String> formatLabelToArray =
             arrayAccession -> MessageFormat.format("ArrayExpress: array design {0}", arrayAccession);
-
-    private static final Function<Experiment, String> formatLinkToExperiment =
-            e -> MessageFormat.format("https://www.ebi.ac.uk/arrayexpress/experiments/{0}", e.getAccession());
-    private static final Function<String, String> formatLinkToArray =
-            arrayAccession -> MessageFormat.format("https://www.ebi.ac.uk/arrayexpress/arrays/{0}", arrayAccession);
 
     private static final Function<String, ExternallyAvailableContent.Description> createIcon =
             label -> ExternallyAvailableContent.Description.create("icon-ae", label);
@@ -41,42 +61,52 @@ public abstract class LinkToArrayExpress<E extends Experiment> extends Externall
 
     @Override
     public Collection<ExternallyAvailableContent> get(E experiment) {
-        return singleton(
-                new ExternallyAvailableContent(
-                        formatLinkToExperiment.apply(experiment),
-                        createIconForExperiment.apply(experiment)));
+         return Stream.of(experiment.getAccession())
+                 .map(EXPERIMENTS_URI_BUILDER::build)
+                 .filter(LinkToArrayExpress::isUriValid)
+                 .map(uri -> new ExternallyAvailableContent(
+                        uri.toString(),
+                        createIconForExperiment.apply(experiment)))
+                 .collect(toImmutableList());
     }
 
-    @Named
-    public static class ProteomicsBaseline extends LinkToArrayExpress<BaselineExperiment> {
-        // In the future we will source proteomics experiments from PRIDE, and one requirement is that they are not
-        // backported into ArrayExpress. E-PROT-6 is an example experiment from PRIDE not in AE.
-        private static final Collection<String> PROTEOMICS_EXPERIMENTS_S_IN_AE =
-                ImmutableList.of("E-PROT-1", "E-PROT-3", "E-PROT-5");
+    @Component
+    public static class ProteomicsBaseline extends LinkToArrayExpress<BaselineExperiment> {}
 
-        @Override
-        public Collection<ExternallyAvailableContent> get(BaselineExperiment experiment) {
-            return PROTEOMICS_EXPERIMENTS_S_IN_AE.contains(experiment.getAccession()) ?
-                    super.get(experiment) :
-                    ImmutableList.of();
-        }
-    }
-
-    @Named
+    @Component
     public static class RnaSeqBaseline extends LinkToArrayExpress<BaselineExperiment> {}
 
-    @Named
+    @Component
     public static class Differential extends LinkToArrayExpress<DifferentialExperiment> {}
 
-    @Named
+    @Component
     public static class Microarray extends LinkToArrayExpress<MicroarrayExperiment> {
         @Override
         public Collection<ExternallyAvailableContent> get(MicroarrayExperiment experiment) {
-            return experiment.getArrayDesignAccessions().stream()
-                    .map(arrayDesignAccession -> new ExternallyAvailableContent(
-                            formatLinkToArray.apply(arrayDesignAccession),
-                            createIconForArray.apply(arrayDesignAccession)))
+            return Stream.concat(
+                    super.get(experiment).stream(),
+                    experiment.getArrayDesignAccessions().stream()
+                            .parallel()
+                            .map(accession -> Pair.of(ARRAYS_URI_BUILDER.build(accession), accession))
+                            .filter(uriAccession -> isUriValid(uriAccession.getLeft()))
+                            .map(uriAccession -> new ExternallyAvailableContent(
+                                    uriAccession.getLeft().toString(),
+                                    createIconForArray.apply(uriAccession.getRight()))))
                     .collect(toImmutableList());
+        }
+    }
+
+    private static boolean isUriValid(@NotNull URI uri) {
+        try {
+            return !webClient
+                    .get()
+                    .uri(uri)
+                    .exchange()
+                    .block()
+                    .statusCode()
+                    .isError();
+        } catch (Exception e) {
+            return false;
         }
     }
 }
