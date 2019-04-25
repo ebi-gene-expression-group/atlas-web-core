1a97753aac507bdd86cca902cadafa443b89c613
Monica Jianu <monica@ebi.ac.uk>
2019-04-25 16:48:43 +0100

    Removed display name field from SingleCellAnalyticsSchemaField as it was no longer needed.

--- src/main/java/uk/ac/ebi/atlas/solr/cloud/collections/SingleCellAnalyticsCollectionProxy.java
+++ src/main/java/uk/ac/ebi/atlas/solr/cloud/collections/SingleCellAnalyticsCollectionProxy.java
@@ -9,30 +9,23 @@ import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
 public class SingleCellAnalyticsCollectionProxy extends CollectionProxy {
 
     public static final class SingleCellAnalyticsSchemaField extends SchemaField<SingleCellAnalyticsCollectionProxy> {
-        private String displayName;
-
-        private SingleCellAnalyticsSchemaField(String fieldName, String displayName) {
+        private SingleCellAnalyticsSchemaField(String fieldName) {
             super(fieldName);
-            this.displayName = displayName;
-        }
-
-        public String displayName() {
-            return displayName;
         }
     }
 
     public static final SingleCellAnalyticsSchemaField CELL_ID =
-            new SingleCellAnalyticsSchemaField("cell_id", "Cell ID");
+            new SingleCellAnalyticsSchemaField("cell_id");
     public static final SingleCellAnalyticsSchemaField EXPERIMENT_ACCESSION =
-            new SingleCellAnalyticsSchemaField("experiment_accession", "Experiment accession");
+            new SingleCellAnalyticsSchemaField("experiment_accession");
     public static final SingleCellAnalyticsSchemaField FACTOR_NAME =
-            new SingleCellAnalyticsSchemaField("factor_name", "Factor name");
+            new SingleCellAnalyticsSchemaField("factor_name");
     public static final SingleCellAnalyticsSchemaField FACTOR_VALUE =
-            new SingleCellAnalyticsSchemaField("factor_value", "Factor value");
+            new SingleCellAnalyticsSchemaField("factor_value");
     public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_NAME =
-            new SingleCellAnalyticsSchemaField("characteristic_name", "Characteristic name");
+            new SingleCellAnalyticsSchemaField("characteristic_name");
     public static final SingleCellAnalyticsSchemaField CHARACTERISTIC_VALUE =
-            new SingleCellAnalyticsSchemaField("characteristic_value", "Characteristic value");
+            new SingleCellAnalyticsSchemaField("characteristic_value");
 
     public SingleCellAnalyticsCollectionProxy(SolrClient solrClient) {
         // scxa-analytics is an alias that points at scxa-analytics-vX
