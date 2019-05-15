a49ddfca03678a729a5c0d8b7a2a1e1d46a83076
Monica Jianu <monica@ebi.ac.uk>
2019-05-15 10:01:43 +0100

    Added tests for response util method.

--- src/test/java/uk/ac/ebi/atlas/solr/cloud/search/SolrQueryResponseUtilsTest.java
+++ src/test/java/uk/ac/ebi/atlas/solr/cloud/search/SolrQueryResponseUtilsTest.java
@@ -1,10 +1,12 @@
 package uk.ac.ebi.atlas.solr.cloud.search;
 
 import org.apache.solr.common.util.SimpleOrderedMap;
+import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,5 +29,48 @@ class SolrQueryResponseUtilsTest {
     void extractingMapsFromNonArrayObjectReturnsEmptyList() {
         assertThat(SolrQueryResponseUtils.extractSimpleOrderedMaps(new Object()))
                 .hasSize(0);
+    }
+
+    @Test
+    void getValuesForValidFacetField() {
+        String facetFieldName = "foo";
+        String facetValue = "bar";
+
+        var solrFacetResponse = getDummySolrFacetResponse(facetFieldName, facetValue);
+
+        assertThat(SolrQueryResponseUtils.getValuesForFacetField(solrFacetResponse, facetFieldName))
+                .containsExactly(facetValue);
+    }
+
+    @Test
+    void getValuesForInvalidFacetField() {
+        String facetFieldName = "foo";
+        String facetValue = "bar";
+        String invalidFacetFieldName = "foobar";
+
+        var solrFacetResponse = getDummySolrFacetResponse(facetFieldName, facetValue);
+
+        assertThat(SolrQueryResponseUtils.getValuesForFacetField(solrFacetResponse, invalidFacetFieldName))
+                .isEmpty();
+    }
+
+    private SimpleOrderedMap getDummySolrFacetResponse(String facetFieldName, String... facetFieldValues) {
+        List<SimpleOrderedMap<Object>> listOfFacets =
+                Arrays.asList(facetFieldValues)
+                        .stream()
+                        .map(value -> {
+                            var facetMap = new SimpleOrderedMap<>();
+                            facetMap.add("val", value);
+                            return facetMap;
+                        })
+                        .collect(Collectors.toList());
+
+        SimpleOrderedMap<List> buckets = new SimpleOrderedMap<>();
+        buckets.add("buckets", listOfFacets);
+
+        SimpleOrderedMap<SimpleOrderedMap> map = new SimpleOrderedMap<>();
+        map.add(facetFieldName, buckets);
+
+        return map;
     }
 }
