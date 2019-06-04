6aea7bda840dd0bcc5ebc325477da77ccfe7f0a5
amunozpomer <amunoz@ebi.ac.uk>
2019-06-04 15:33:15 +0100

    Make API more general by accepting Collections both for collection names and aliases

--- src/test/java/uk/ac/ebi/atlas/monitoring/HealthCheckServiceTest.java
+++ src/test/java/uk/ac/ebi/atlas/monitoring/HealthCheckServiceTest.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.atlas.monitoring;
 
+import com.google.common.collect.ImmutableSet;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,17 +11,13 @@ import uk.ac.ebi.atlas.experimentimport.ExperimentDao;
 import uk.ac.ebi.atlas.solr.cloud.admin.SolrCloudAdminProxy;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyCollection;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class HealthCheckServiceTest {
-
     @Mock
     private SolrCloudAdminProxy solrCloudAdminProxyMock;
 
@@ -29,8 +26,9 @@ class HealthCheckServiceTest {
 
     private HealthCheckService subject;
 
-    private static final List<String> MOCK_SOLR_COLLECTIONS = Arrays.asList("mockCollection1", "mockCollection2");
-    private static final String MOCK_SOLR_COLLECTION_ALIAS = "mockCollectionAlias";
+    private static final ImmutableSet<String> MOCK_SOLR_COLLECTIONS =
+            ImmutableSet.of("mockCollection1", "mockCollection2");
+    private static final ImmutableSet<String> MOCK_SOLR_COLLECTION_ALIAS = ImmutableSet.of("mockCollectionAlias");
 
     @BeforeEach
     void setUp() {
@@ -39,43 +37,38 @@ class HealthCheckServiceTest {
 
     @Test
     void solrCollectionsAreUp() throws IOException, SolrServerException {
-        when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenReturn(true);
-
+        when(solrCloudAdminProxyMock.areCollectionsUp(anyCollection(), anyCollection())).thenReturn(true);
         assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isTrue();
     }
 
     @Test
     void solrCollectionsAreDown() throws IOException, SolrServerException {
-        when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenReturn(false);
-
+        when(solrCloudAdminProxyMock.areCollectionsUp(anyCollection(), anyCollection())).thenReturn(false);
         assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isFalse();
     }
 
     @Test
     void solrThrowsException() throws IOException, SolrServerException {
-        when(solrCloudAdminProxyMock.areCollectionsUp(anyList(), any())).thenThrow(RuntimeException.class);
-
+        when(solrCloudAdminProxyMock.areCollectionsUp(anyCollection(), anyCollection()))
+                .thenThrow(RuntimeException.class);
         assertThat(subject.isSolrUp(MOCK_SOLR_COLLECTIONS, MOCK_SOLR_COLLECTION_ALIAS)).isFalse();
     }
 
     @Test
     void experimentsDatabaseIsUp() {
         when(experimentDaoMock.countExperiments()).thenReturn(9);
-
         assertThat(subject.isDatabaseUp(experimentDaoMock)).isTrue();
     }
 
     @Test
     void noExperimentsInDatabase() {
         when(experimentDaoMock.countExperiments()).thenReturn(0);
-
         assertThat(subject.isDatabaseUp(experimentDaoMock)).isFalse();
     }
 
     @Test
     void experimentDaoThrowsException() {
         when(experimentDaoMock.countExperiments()).thenThrow(RuntimeException.class);
-
         assertThat(subject.isDatabaseUp(experimentDaoMock)).isFalse();
     }
 }
