483be1e0848b05eced20ff3114d965381b53c485
amunozpomer <amunoz@ebi.ac.uk>
2019-08-16 16:05:00 +0100

    Improve ExperimentCrud in several ways: 1. Use ExperimentCrudDao 2. Thin down a lot of unneeded cruft with multiple code paths to get the same data 3. Just leave the necessary collaborators, whatever else is needed will be provided by the specific implementations

--- src/main/java/uk/ac/ebi/atlas/experimentimport/ExperimentCrud.java
+++ src/main/java/uk/ac/ebi/atlas/experimentimport/ExperimentCrud.java
@@ -1,95 +1,53 @@
 package uk.ac.ebi.atlas.experimentimport;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import uk.ac.ebi.atlas.controllers.ResourceNotFoundException;
-import uk.ac.ebi.atlas.experimentimport.condensedSdrf.CondensedSdrfParser;
+import com.google.common.collect.ImmutableList;
 import uk.ac.ebi.atlas.experimentimport.experimentdesign.ExperimentDesignFileWriterService;
-import uk.ac.ebi.atlas.experimentimport.idf.IdfParser;
 import uk.ac.ebi.atlas.model.experiment.ExperimentDesign;
-import uk.ac.ebi.atlas.trader.ConfigurationTrader;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.text.MessageFormat;
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-import static com.google.common.base.Preconditions.checkState;
-
-// Inserts experiment in (scxa_)experiment table and writes the experiment design file to expdesign/
 public abstract class ExperimentCrud {
-    private static final Logger LOGGER = LoggerFactory.getLogger(ExperimentCrud.class);
+    protected final ExperimentCrudDao experimentCrudDao;
+    private final ExperimentDesignFileWriterService experimentDesignFileWriterService;
 
-    protected final ExperimentDao experimentDao;
-    protected final ExperimentChecker experimentChecker;
-    protected final ExperimentDesignFileWriterService experimentDesignFileWriterService;
-    protected final CondensedSdrfParser condensedSdrfParser;
-    protected final IdfParser idfParser;
-
-    public ExperimentCrud(ExperimentDao experimentDao,
-                          ExperimentChecker experimentChecker,
-                          CondensedSdrfParser condensedSdrfParser,
-                          IdfParser idfParser,
+    public ExperimentCrud(ExperimentCrudDao experimentCrudDao,
                           ExperimentDesignFileWriterService experimentDesignFileWriterService) {
-
-        this.experimentDao = experimentDao;
-        this.experimentChecker = experimentChecker;
-        this.condensedSdrfParser = condensedSdrfParser;
-        this.idfParser = idfParser;
+        this.experimentCrudDao = experimentCrudDao;
         this.experimentDesignFileWriterService = experimentDesignFileWriterService;
     }
 
-    public abstract UUID importExperiment(String experimentAccession, boolean isPrivate);
-    public abstract void updateExperimentDesign(String experimentAccession);
+    // Create
+    public abstract UUID createExperiment(String experimentAccession, boolean isPrivate);
 
-    public ExperimentDto findExperiment(String experimentAccession) {
-        return experimentDao.getExperimentAsAdmin(experimentAccession);
+    // Read
+    public Optional<ExperimentDto> readExperiment(String experimentAccession) {
+        return Optional.ofNullable(experimentCrudDao.readExperiment(experimentAccession));
     }
 
-    public List<ExperimentDto> findAllExperiments() {
-        return experimentDao.getAllExperimentsAsAdmin();
+    public ImmutableList<ExperimentDto> readExperiments() {
+        return experimentCrudDao.readExperiments();
     }
 
-    public void makeExperimentPrivate(String experimentAccession) {
-        setExperimentPrivacyStatus(experimentAccession, true);
+    // Update
+    public void updateExperimentPrivate(String experimentAccession, boolean isPrivate) {
+        experimentCrudDao.updateExperimentPrivate(experimentAccession, isPrivate);
     }
 
-    public void makeExperimentPublic(String experimentAccession) {
-        setExperimentPrivacyStatus(experimentAccession, false);
-    }
-
+    // Delete
     public void deleteExperiment(String experimentAccession) {
-        var experimentDTO = findExperiment(experimentAccession);
-        checkNotNull(experimentDTO, MessageFormat.format("Experiment not found: {0}", experimentAccession));
-        experimentDao.deleteExperiment(experimentDTO.getExperimentAccession());
+        experimentCrudDao.deleteExperiment(experimentAccession);
     }
 
-    private void setExperimentPrivacyStatus(String experimentAccession, boolean newPrivacyStatus) {
-        experimentDao.setExperimentPrivacyStatus(experimentAccession, newPrivacyStatus);
-        checkState(
-                newPrivacyStatus == experimentDao.getExperimentAsAdmin(experimentAccession).isPrivate(),
-                "Failed to change is_private column in the DB! (?)");
-    }
-
-    protected Optional<String> fetchExperimentAccessKey(String experimentAccession) {
-        try {
-            var experiment = findExperiment(experimentAccession);
-            return Optional.of(experiment.getAccessKey());
-        } catch (ResourceNotFoundException e) {
-            return Optional.empty();
-        }
-    }
-
-    protected void updateWithNewExperimentDesign(ExperimentDesign newDesign, ExperimentDto experimentDTO) {
+    public abstract void updateExperimentDesign(String experimentAccession);
+    protected void updateExperimentDesign(ExperimentDesign experimentDesign, ExperimentDto experimentDto) {
         try {
             experimentDesignFileWriterService.writeExperimentDesignFile(
-                    experimentDTO.getExperimentAccession(),
-                    experimentDTO.getExperimentType(),
-                    newDesign);
-            LOGGER.info("updated design for experiment {}", experimentDTO.getExperimentAccession());
+                    experimentDto.getExperimentAccession(),
+                    experimentDto.getExperimentType(),
+                    experimentDesign);
         } catch (IOException e) {
             throw new UncheckedIOException(e);
         }
