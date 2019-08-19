901e6d08e9fb484494139fa0a73d4f769aeb4881
amunozpomer <amunoz@ebi.ac.uk>
2019-08-19 16:49:47 +0100

    Replace ExperimentTrader with a common shared class for bulk and SC and move the interface to ExperimentRepository, which will be implemented in each project with cache and all necessary bells and whistles

--- src/main/java/uk/ac/ebi/atlas/trader/ExperimentTrader.java
+++ src/main/java/uk/ac/ebi/atlas/trader/ExperimentTrader.java
@@ -2,17 +2,55 @@ package uk.ac.ebi.atlas.trader;
 
 import com.google.common.collect.ImmutableSet;
 import org.springframework.stereotype.Component;
+import uk.ac.ebi.atlas.controllers.ResourceNotFoundException;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
 import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
+
 @Component
-public interface ExperimentTrader {
+public class ExperimentTrader {
+    private final ExperimentTraderDao experimentTraderDao;
+    private final ExperimentRepository experimentRepository;
+
+    public ExperimentTrader(ExperimentTraderDao experimentTraderDao,
+                            ExperimentRepository experimentRepository) {
+        this.experimentTraderDao = experimentTraderDao;
+        this.experimentRepository = experimentRepository;
+    }
+
     // Under most circumstances you should use getExperiment(experimentAccession, accessKey). This method will return
     // any experiment, public or private, disregarding the private flag and without requiring the access key.
     // Use with care!
-    Experiment getExperimentForAnalyticsIndex(String experimentAccession);
+    public Experiment getExperimentForAnalyticsIndex(String experimentAccession) {
+        return experimentRepository.getExperiment(experimentAccession);
+    }
 
-    Experiment getPublicExperiment(String experimentAccession);
-    Experiment getExperiment(String experimentAccession, String accessKey);
-    ImmutableSet<Experiment> getPublicExperiments(ExperimentType... types);
+    public Experiment getPublicExperiment(String experimentAccession) {
+        var experiment = experimentRepository.getExperiment(experimentAccession);
+
+        if (!experiment.isPrivate()) {
+            return experiment;
+        }
+
+        throw new ResourceNotFoundException(
+                "Public experiment " + experimentAccession + " could not be found");
+    }
+
+    public Experiment getExperiment(String experimentAccession, String accessKey) {
+        var experiment = experimentRepository.getExperiment(experimentAccession);
+
+        if (experiment.getAccessKey().equalsIgnoreCase(accessKey)) {
+            return experiment;
+        }
+
+        throw new ResourceNotFoundException(
+                "Experiment " + experimentAccession + " could not be found or bad access key");
+    }
+
+    public ImmutableSet<Experiment> getPublicExperiments(ExperimentType... types) {
+        return experimentTraderDao.fetchPublicExperimentAccessions(types).stream()
+                .map(this::getPublicExperiment)
+                .collect(toImmutableSet());
+    }
 }
