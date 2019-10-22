e4fb74839a3cb85aaa6e9b6e2ee104472a48ca4e
Haider Iqbal <haideri@ebi.ac.uk>
2019-10-22 16:20:40 +0100

    - Revert to old logic

--- src/main/java/uk/ac/ebi/atlas/experiments/ExperimentInfoListService.java
+++ src/main/java/uk/ac/ebi/atlas/experiments/ExperimentInfoListService.java
@@ -2,11 +2,9 @@ package uk.ac.ebi.atlas.experiments;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.model.experiment.Experiment;
-import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 import uk.ac.ebi.atlas.trader.ExperimentTrader;
 import uk.ac.ebi.atlas.utils.ExperimentInfo;
 
@@ -37,10 +35,7 @@ public class ExperimentInfoListService {
 
     public JsonObject getExperimentsJson() {
         // TODO We can remove aaData when https://www.pivotaltracker.com/story/show/165720572 is done
-        JsonObject result = new JsonObject();
-        result.add("dropdownFilters", getExperimentFilterDropdowns());
-        result.add("aaData", GSON.toJsonTree(ImmutableMap.of("aaData", listPublicExperiments())).getAsJsonObject());
-        return result;
+        return  GSON.toJsonTree(ImmutableMap.of("aaData", listPublicExperiments())).getAsJsonObject();
     }
 
     public ImmutableList<ExperimentInfo> listPublicExperiments() {
@@ -60,71 +55,6 @@ public class ExperimentInfoListService {
                                 experimentTypePrecedenceList.indexOf(experiment.getType()))
                         .thenComparing(Experiment::getDisplayName))
                 .map(Experiment::buildExperimentInfo)
-                .collect(toImmutableList());
-    }
-
-    private JsonArray getExperimentFilterDropdowns() {
-        var dropdownFilters = new JsonArray();
-        var projectOptions = new JsonObject();
-        var kingdomOptions = new JsonObject();
-        var technologyTypeOptions = new JsonObject();
-        var experimentTypeOptions = new JsonObject();
-
-        if(getProjectOptions().size() != 0) {
-            projectOptions.add("label", GSON.toJsonTree("Project"));
-            projectOptions.add("options", GSON.toJsonTree(getProjectOptions()));
-            dropdownFilters.add(projectOptions);
-        }
-
-        if(getKingdomOptions().size() != 0) {
-            kingdomOptions.add("label", GSON.toJsonTree("Kingdom"));
-            kingdomOptions.add("options", GSON.toJsonTree(getKingdomOptions()));
-            dropdownFilters.add(kingdomOptions);
-        }
-
-        if(getTechnologyTypeOptions().size() != 0) {
-            technologyTypeOptions.add("label", GSON.toJsonTree("Technology Type"));
-            technologyTypeOptions.add("options", GSON.toJsonTree(getTechnologyTypeOptions()));
-            dropdownFilters.add(technologyTypeOptions);
-
-        }
-
-        if(getExperimentTypeOptions().size() != 0) {
-            experimentTypeOptions.add("label", GSON.toJsonTree("Experiment Type"));
-            experimentTypeOptions.add("options", GSON.toJsonTree(getExperimentTypeOptions()));
-            dropdownFilters.add(experimentTypeOptions);
-        }
-
-        return dropdownFilters;
-    }
-
-    private ImmutableList<String> getKingdomOptions() {
-        return listPublicExperiments().stream()
-                .map(ExperimentInfo::getKingdom)
-                .distinct()
-                .collect(toImmutableList());
-    }
-
-    private ImmutableList<ExperimentType> getExperimentTypeOptions() {
-        return listPublicExperiments().stream()
-                .map(ExperimentInfo::getExperimentType)
-                .distinct()
-                .collect(toImmutableList());
-    }
-
-    private ImmutableList<String> getProjectOptions() {
-        return  listPublicExperiments().stream()
-                .map(ExperimentInfo::getExperimentProjects)
-                .flatMap(project -> project.stream())
-                .distinct()
-                .collect(toImmutableList());
-    }
-
-    private ImmutableList<String> getTechnologyTypeOptions() {
-        return listPublicExperiments().stream()
-                .map(ExperimentInfo::getTechnologyType)
-                .flatMap(technologyType -> technologyType.stream())
-                .distinct()
                 .collect(toImmutableList());
     }
 }
