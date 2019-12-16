e9a2f6461d5c9de8b4d2358c4bf51b56924306ec
amunozpomer <amunoz@ebi.ac.uk>
2019-12-16 17:03:03 +0000

    Return a single card of HCA experiments for the HCA landing page

--- src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
+++ src/main/java/uk/ac/ebi/atlas/model/card/CardModelFactory.java
@@ -4,16 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.home.species.SpeciesSummary;
-import uk.ac.ebi.atlas.model.experiment.Experiment;
-import uk.ac.ebi.atlas.species.Species;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
-import static uk.ac.ebi.atlas.utils.UrlHelpers.getExperimentUrl;
 import static uk.ac.ebi.atlas.utils.UrlHelpers.getExperimentsFilteredBySpeciesAndExperimentType;
 import static uk.ac.ebi.atlas.utils.UrlHelpers.getExperimentsFilteredBySpeciesUrl;
 
@@ -50,41 +45,5 @@ public class CardModelFactory {
                         Optional.of(StringUtils.capitalize(speciesSummary.getSpecies())),
                         Optional.of(getExperimentsFilteredBySpeciesUrl(speciesSummary.getSpecies()))),
                 content);
-    }
-
-    public CardModel createLandingPageSpeciesCard(Collection<Experiment> experiments) {
-        Species species = experiments.iterator().next().getSpecies();
-
-        return CardModel.create(
-                CardIconType.SPECIES,
-                species.getReferenceName(),
-                Pair.of(
-                        Optional.of(StringUtils.capitalize(species.getName() + " experiments")),
-                        Optional.of(getExperimentsFilteredBySpeciesUrl(species.getReferenceName()))),
-                experimentsToContent(experiments));
-    }
-
-    public CardModel createLandingPageImageCard(Collection<Experiment> experiments,
-                                                String imageUrl,
-                                                String description,
-                                                String descriptionUrl) {
-        return CardModel.create(
-                CardIconType.IMAGE,
-                imageUrl,
-                Pair.of(Optional.of(description), Optional.of(descriptionUrl)),
-                experimentsToContent(experiments));
-    }
-
-    private List<Pair<String, Optional<String>>> experimentsToContent(Collection<Experiment> experiments) {
-        return experiments
-                .stream()
-                .map(this::experimentToDescriptionAndUrl)
-                .collect(Collectors.toList());
-    }
-
-    private Pair<String, Optional<String>> experimentToDescriptionAndUrl(Experiment experiment) {
-        return Pair.of(
-                experiment.getDescription(),
-                Optional.of(getExperimentUrl(experiment)));
     }
 }
