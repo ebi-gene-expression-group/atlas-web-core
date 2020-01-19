6c930a56c84c37361e86b59bfe175f2aa34cf8d6
amunozpomer <amunoz@ebi.ac.uk>
2020-01-19 20:12:29 +0000

    Bugfix #170789143 – HCA landing page links are broken (#33)

--- src/main/java/uk/ac/ebi/atlas/utils/UrlHelpers.java
+++ src/main/java/uk/ac/ebi/atlas/utils/UrlHelpers.java
@@ -2,15 +2,35 @@ package uk.ac.ebi.atlas.utils;
 
 import org.apache.commons.lang3.tuple.Pair;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-import org.springframework.web.util.UriComponentsBuilder;
-import uk.ac.ebi.atlas.model.experiment.Experiment;
 
 import java.util.Optional;
 
 public class UrlHelpers {
+     public UrlHelpers() {
+         throw new UnsupportedOperationException();
+     }
+
+    private static String getExperimentUrl(String accession) {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path("/experiments/{accession}")
+                .buildAndExpand(accession)
+                .toUriString();
+    }
+
+    private static String getExperimentUrl(String host, String accession) {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .scheme("https")
+                .host(host)
+                .path("/experiments/{accession}")
+                .buildAndExpand(accession)
+                .toUriString();
+    }
+
     public static String getExperimentsFilteredBySpeciesUrl(String species) {
-        return UriComponentsBuilder.newInstance()
-                .path(getApplicationContext())
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
                 .path("/experiments")
                 .query("species={species}")
                 .buildAndExpand(species)
@@ -18,21 +38,9 @@ public class UrlHelpers {
                 .toUriString();
     }
 
-    public static String getExperimentUrl(Experiment experiment) {
-        return getExperimentUrl(experiment.getAccession());
-    }
-
-    public static String getExperimentUrl(String accession) {
-        return UriComponentsBuilder.newInstance()
-                .path(getApplicationContext())
-                .path("/experiments/{accession}")
-                .buildAndExpand(accession)
-                .toUriString();
-    }
-
     public static String getExperimentsFilteredBySpeciesAndExperimentType(String species, String type) {
-        return UriComponentsBuilder.newInstance()
-                .path(getApplicationContext())
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
                 .path("/experiments")
                 .query("species={species}")
                 .query("experimentType={type}")
@@ -42,23 +50,34 @@ public class UrlHelpers {
     }
 
     public static String getExperimentsSummaryImageUrl(String imageFileName) {
-        return UriComponentsBuilder.newInstance()
-                .path(getApplicationContext())
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
                 .path("/resources/images/experiments-summary/{imageFileName}.png")
                 .buildAndExpand(imageFileName)
                 .toUriString();
     }
 
     public static String getCustomUrl(String path) {
-        return UriComponentsBuilder.newInstance()
-                .path(getApplicationContext())
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path(path)
+                .build()
+                .toUriString();
+    }
+
+    public static String getCustomUrl(String host, String path) {
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .scheme("https")
+                .host(host)
                 .path(path)
                 .build()
                 .toUriString();
     }
 
     private static String getExperimentSetUrl(String keyword) {
-        return ServletUriComponentsBuilder.fromCurrentContextPath()
+        return ServletUriComponentsBuilder
+                .fromCurrentContextPath()
                 .path("/experiments")
                 .query("experimentSet={keyword}")
                 .buildAndExpand(keyword)
@@ -73,15 +92,15 @@ public class UrlHelpers {
         return Pair.of(label, Optional.of(getExperimentUrl(accession)));
     }
 
+    public static Pair<String, Optional<String>> getExperimentLink(String host, String label, String accession) {
+        return Pair.of(label, Optional.of(getExperimentUrl(host, accession)));
+    }
+
     public static Pair<Optional<String>, Optional<String>> getLinkWithEmptyLabel(String link) {
         return Pair.of(Optional.empty(), Optional.of(link));
     }
 
     public static Pair<Optional<String>, Optional<String>> getExperimentLink(String accession) {
         return getLinkWithEmptyLabel(getExperimentUrl(accession));
-    }
-
-    private static String getApplicationContext() {
-        return Optional.ofNullable(ServletUriComponentsBuilder.fromCurrentContextPath().build().getPath()).orElse("");
     }
 }
