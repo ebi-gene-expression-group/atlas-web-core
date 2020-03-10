9a548b3fb869a1b33fc9b1bc8cbb00b270db109c
Haider Iqbal <haideri@ebi.ac.uk>
2020-03-10 17:32:09 +0000

    Add new constructor for solr select stream (#43)
    * - Add new constructor for solr select stream
    
    * - Refactor code to use ImmutableList builder

--- src/main/java/uk/ac/ebi/atlas/solr/cloud/search/streamingexpressions/decorator/SelectStreamBuilder.java
+++ src/main/java/uk/ac/ebi/atlas/solr/cloud/search/streamingexpressions/decorator/SelectStreamBuilder.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.atlas.solr.cloud.search.streamingexpressions.decorator;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import org.apache.solr.client.solrj.io.stream.SelectStream;
 import org.apache.solr.client.solrj.io.stream.TupleStream;
@@ -11,10 +12,16 @@ import java.util.Map;
 
 public class SelectStreamBuilder extends TupleStreamBuilder {
     private final TupleStreamBuilder tupleStreamBuilder;
+    private final ImmutableList.Builder<String> selectedFieldsBuilder = ImmutableList.builder();
     private final ImmutableMap.Builder<String, String> fieldMapsBuilder = ImmutableMap.builder();
 
     public SelectStreamBuilder(TupleStreamBuilder tupleStreamBuilder) {
         this.tupleStreamBuilder = tupleStreamBuilder;
+    }
+
+    public SelectStreamBuilder(TupleStreamBuilder tupleStreamBuilder, ImmutableList<String> selectedFieldsBuilder) {
+        this.tupleStreamBuilder = tupleStreamBuilder;
+        this.selectedFieldsBuilder.addAll(selectedFieldsBuilder);
     }
 
     // public SelectStreamBuilder<T> addFieldMap(String originalfieldName, String newFieldName) {
@@ -30,7 +37,9 @@ public class SelectStreamBuilder extends TupleStreamBuilder {
     @Override
     protected TupleStream getRawTupleStream() {
         try {
-            return new SelectStream(tupleStreamBuilder.build(), fieldMapsBuilder.build());
+            return selectedFieldsBuilder.build().isEmpty() ?
+                    new SelectStream(tupleStreamBuilder.build(), fieldMapsBuilder.build()) :
+                    new SelectStream(tupleStreamBuilder.build(), selectedFieldsBuilder.build());
         } catch (IOException e) {
             // The truth is, this constructor can’t throw an IOException, but the class declaration does... lame
             throw new UncheckedIOException(e);
