3267e7f187b67f999f8d3ee48fdb690c90a7d712
upendra kumbham <ukumbham@ebi.ac.uk>
2021-04-06 08:22:54 +0100

    Add interfaces
    We are going with interfaces approach for the suggesters implementation as we are planning to implement suggesters even in the bulk. So we can implement same interfaces in the bulk as well if we maintain interfaces in the core module.

---
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterDao.java
@@ -0,0 +1,14 @@
+package uk.ac.ebi.atlas.search.suggester;
+
+import org.apache.solr.client.solrj.response.Suggestion;
+import uk.ac.ebi.atlas.species.Species;
+
+import java.util.stream.Stream;
+
+public interface AnalyticsSuggesterDao {
+
+    Stream<Suggestion> fetchOntologyAnnotationSuggestions(String query, int limit, Species... species);
+
+    Stream<Suggestion> fetchOntologySuggestions(String query, int limit, Species... species);
+
+}
---
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterService.java
@@ -0,0 +1,9 @@
+package uk.ac.ebi.atlas.search.suggester;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+public interface AnalyticsSuggesterService {
+
+    Stream<Map<String, String>> fetchOntologyAnnotationSuggestions(String query, String... species);
+}
