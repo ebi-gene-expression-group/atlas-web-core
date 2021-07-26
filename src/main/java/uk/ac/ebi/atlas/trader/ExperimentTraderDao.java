933be61e7d28c7756a550730b8c2d326c373715d
upendra kumbham <upendra.kumbham@gmail.com>
2021-07-26 12:03:41 +0100

    Cache all private experiment accessions

--- src/main/java/uk/ac/ebi/atlas/trader/ExperimentTraderDao.java
+++ src/main/java/uk/ac/ebi/atlas/trader/ExperimentTraderDao.java
@@ -2,10 +2,9 @@ package uk.ac.ebi.atlas.trader;
 
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 import uk.ac.ebi.atlas.model.experiment.ExperimentType;
 import java.util.stream.Stream;
 
@@ -32,6 +31,7 @@ public class ExperimentTraderDao {
                         String.class));
     }
 
+    @Cacheable("experimentAccessions")
     public ImmutableSet<String> fetchPrivateExperimentAccessions(){
         return ImmutableSet.copyOf(
                 namedParameterJdbcTemplate.queryForList(
