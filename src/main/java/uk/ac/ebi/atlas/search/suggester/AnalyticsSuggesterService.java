03ec3ca6217d6bbf477f048e8348c541e252ba75
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-09-09 09:52:28 +0100

    Add @Service annotation

--- src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterService.java
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterService.java
@@ -1,9 +1,11 @@
 package uk.ac.ebi.atlas.search.suggester;
 
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 import java.util.stream.Stream;
 
+@Service
 public interface AnalyticsSuggesterService {
-
     Stream<Map<String, String>> fetchOntologyAnnotationSuggestions(String query, String... species);
 }
