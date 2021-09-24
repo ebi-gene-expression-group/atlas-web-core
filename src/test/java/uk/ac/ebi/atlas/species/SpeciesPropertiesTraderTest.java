1c1485e10697cbc8629e500864bb17435eb0be1c
Alfonso Muñoz-Pomer Fuentes <amunoz@ebi.ac.uk>
2021-09-24 21:25:42 +0100

    Unknown species should at least get a normalised name

--- src/test/java/uk/ac/ebi/atlas/species/SpeciesPropertiesTraderTest.java
+++ src/test/java/uk/ac/ebi/atlas/species/SpeciesPropertiesTraderTest.java
@@ -1,75 +1,71 @@
 package uk.ac.ebi.atlas.species;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.apache.commons.io.IOUtils.toInputStream;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.greaterThan;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.not;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateBlankString;
 
 public class SpeciesPropertiesTraderTest {
-
     private Path location;
-
     private int speciesPropertiesCountBeforeRefresh;
 
-    private SpeciesPropertiesTrader subject = new SpeciesPropertiesTrader();
+    private SpeciesPropertiesTrader subject;
 
-    @Before
+    @BeforeEach
     public void setUp() throws IOException {
+        subject = new SpeciesPropertiesTrader();
         location =
                 createSpeciesPropertiesFile(
                         SpeciesPropertiesTraderTest.class.getResourceAsStream("species-properties.json"));
-        SpeciesPropertiesDao speciesPropertiesDao = new SpeciesPropertiesDao(location);
-        subject.setSpeciesPropertiesDao(speciesPropertiesDao);
+        subject.setSpeciesPropertiesDao(new SpeciesPropertiesDao(location));
 
         subject.refresh();
         speciesPropertiesCountBeforeRefresh = subject.getAll().size();
     }
 
     @Test
-    public void getAll() {
-        assertThat(subject.getAll(), hasSize(greaterThan(50)));
+    void getAll() {
+        assertThat(subject.getAll().size()).isGreaterThan(50);
     }
 
     @Test
-    public void get() {
-        assertThat(subject.get("Homo sapiens").referenceName(), is("homo sapiens"));
-        assertThat(subject.get("Homo sapiens").ensemblName(), is("Homo_sapiens"));
-        assertThat(subject.get("Homo sapiens").kingdom(), is("animals"));
-        assertThat(subject.get("Hordeum vulgare").getResourcesOfType("genome_browser"), hasSize(2));
+    void get() {
+        assertThat(subject.get("Homo sapiens").referenceName())
+                .isEqualTo("homo sapiens");
+        assertThat(subject.get("Homo sapiens").ensemblName())
+                .isEqualTo("Homo_sapiens");
+        assertThat(subject.get("Homo sapiens").kingdom())
+                .isEqualTo("animals");
+        assertThat(subject.get("Hordeum vulgare").getResourcesOfType("genome_browser")).isNotEmpty();
     }
 
     @Test
-    public void refresh() throws IOException {
+    void refresh() throws IOException {
         Files.copy(
                 SpeciesPropertiesTraderTest.class.getResourceAsStream("species-tyrannosaurus-rex.json"),
                 location,
                 REPLACE_EXISTING);
         subject.refresh();
 
-        assertThat(subject.getAll(), hasSize(1));
-        assertThat(subject.getAll().iterator().next().referenceName(), is("tyrannosaurus rex"));
+        assertThat(subject.getAll()).hasSize(1);
+        assertThat(subject.getAll().iterator().next().referenceName()).isEqualTo("tyrannosaurus rex");
 
     }
 
     @Test
-    public void refreshDescribesChanges() throws IOException {
-        String refreshMessage = subject.refresh();
-        assertThat(refreshMessage, is("No changes were made to the reference species"));
+    void refreshDescribesChanges() throws IOException {
+        var refreshMessage = subject.refresh();
+        assertThat(refreshMessage).isEqualTo("No changes were made to the reference species");
 
         Files.copy(
                 SpeciesPropertiesTraderTest.class.getResourceAsStream("species-tyrannosaurus-rex.json"),
@@ -77,15 +73,15 @@ public class SpeciesPropertiesTraderTest {
                 REPLACE_EXISTING);
         refreshMessage = subject.refresh();
 
-        Pattern removedSpeciesPattern = Pattern.compile(".*\\[(.+)] removed.*");
-        Matcher matcher = removedSpeciesPattern.matcher(refreshMessage);
+        var removedSpeciesPattern = Pattern.compile(".*\\[(.+)] removed.*");
+        var matcher = removedSpeciesPattern.matcher(refreshMessage);
         matcher.find();
-            assertThat(matcher.group(1).split(", ").length, is(speciesPropertiesCountBeforeRefresh));
-        assertThat(refreshMessage, containsString("[Tyrannosaurus_rex] added"));
+        assertThat(matcher.group(1).split(", ")).hasSize(speciesPropertiesCountBeforeRefresh);
+        assertThat(refreshMessage).containsSubsequence("[Tyrannosaurus_rex] added");
     }
 
     @Test
-    public void failedRefreshKeepsOldProperties() {
+    void failedRefreshKeepsOldProperties() {
         try {
             Files.copy(
                     toInputStream("invalid JSON contents", StandardCharsets.UTF_8),
@@ -94,37 +90,46 @@ public class SpeciesPropertiesTraderTest {
             // We should never get here
             throw new RuntimeException();
         } catch (IOException e) {
-            assertThat(subject.getAll(), hasSize(speciesPropertiesCountBeforeRefresh));
+            assertThat(subject.getAll()).hasSize(speciesPropertiesCountBeforeRefresh);
         }
     }
 
     @Test
-    public void unknownSpecies() {
-        assertThat(subject.get("foobar"), is(SpeciesProperties.UNKNOWN));
+    void unknownSpecies() {
+        assertThat(subject.get(generateBlankString())).isEqualTo(SpeciesProperties.UNKNOWN);
+        var namedUnknownSpecies = subject.get("foo bar");
+        assertThat(namedUnknownSpecies.referenceName()).isEqualTo("foo bar");
+        assertThat(namedUnknownSpecies.ensemblName()).isEqualTo("Foo_bar");
+        assertThat(namedUnknownSpecies.kingdom()).isEmpty();
+        assertThat(namedUnknownSpecies.defaultQueryFactorType()).isEmpty();
+        assertThat(namedUnknownSpecies.resources()).isEmpty();
     }
 
     @Test
-    public void speciesNamesAreNormalised() {
-        assertThat(subject.get("homo sapiens"), not(is(SpeciesProperties.UNKNOWN)));
-        assertThat(subject.get("homo sapiens"), is(subject.get("HoMo_SaPieNs")));
-        assertThat(subject.get("homo sapiens"), is(subject.get("Homo sapiens")));
+    void speciesNamesAreNormalised() {
+        assertThat(subject.get("homo sapiens"))
+                .isNotEqualTo(SpeciesProperties.UNKNOWN)
+                .isEqualTo(subject.get("HoMo_SaPieNs"))
+                .isEqualTo(subject.get("Homo sapiens"));
 
-        assertThat(subject.get("hordeum vulgare"), not(is(SpeciesProperties.UNKNOWN)));
-        assertThat(subject.get("hordeum vulgare"), is(subject.get("Hordeum vulgare subsp. vulgare")));
-        assertThat(subject.get("hordeum vulgare"), is(subject.get("Hordeum_vulgare")));
+        assertThat(subject.get("hordeum vulgare"))
+                .isNotEqualTo(SpeciesProperties.UNKNOWN)
+                .isEqualTo(subject.get("Hordeum vulgare subsp. vulgare"))
+                .isEqualTo(subject.get("Hordeum_vulgare"));
     }
 
     @Test
-    public void appliesExceptions() {
-        assertThat(subject.get("canis lupus").referenceName(), is("canis familiaris"));
-        assertThat(subject.get("Canis_lupus").referenceName(), is("canis familiaris"));
-        assertThat(subject.get("canis_lupus_familiaris").referenceName(), is("canis familiaris"));
-        assertThat(subject.get("Canis_lupus familiaris").referenceName(), is("canis familiaris"));
-        assertThat(subject.get("Canis Lupus Familiaris").referenceName(), is("canis familiaris"));
+    void appliesExceptions() {
+        assertThat(subject.get("canis lupus").referenceName())
+                .isEqualTo(subject.get("Canis_lupus").referenceName())
+                .isEqualTo(subject.get("canis_lupus_familiaris").referenceName())
+                .isEqualTo(subject.get("Canis_lupus familiaris").referenceName())
+                .isEqualTo(subject.get("Canis Lupus Familiari").referenceName())
+                .isEqualTo("canis familiaris");
     }
 
     private Path createSpeciesPropertiesFile(InputStream in) throws IOException {
-        Path tempDirPath = Files.createTempDirectory("");
+        var tempDirPath = Files.createTempDirectory("");
         tempDirPath.toFile().deleteOnExit();
         Files.copy(in, tempDirPath.resolve("species-properties.json"));
         return tempDirPath.resolve("species-properties.json");
