bf852aaa56a11e9bfaf071886e14492ac25ef628
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-10-14 10:34:09 +0100

    Update code to make analytics payload format same as bioentity format

--- src/main/java/uk/ac/ebi/atlas/search/suggester/SolrSuggestionReactSelectAdapter.java
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/SolrSuggestionReactSelectAdapter.java
@@ -3,19 +3,24 @@ package uk.ac.ebi.atlas.search.suggester;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonArray;
+import uk.ac.ebi.atlas.solr.analytics.AnalyticsPropertyName;
 import uk.ac.ebi.atlas.utils.GsonProvider;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
+import static uk.ac.ebi.atlas.solr.analytics.AnalyticsPropertyName.METADATA;
 import static uk.ac.ebi.atlas.solr.cloud.collections.BioentitiesCollectionProxy.ID_PROPERTY_NAMES;
 
 public class SolrSuggestionReactSelectAdapter {
+    // Where, and in what order, should we search in case of a free text query (without category)
+    public static final ImmutableList<AnalyticsPropertyName> ANALYTICS_SUGGESTER_NAMES_FOR_METADATA_SEARCH =
+            ImmutableList.of(METADATA);
+
     protected SolrSuggestionReactSelectAdapter() {
         throw new UnsupportedOperationException();
     }
@@ -48,15 +53,31 @@ public class SolrSuggestionReactSelectAdapter {
         return jsonArray;
     }
 
-    public static Map<String, List<ImmutableList<String>>> metaDataSerialize(Stream<Map<String, String>> suggestions) {
+    public static JsonArray metaDataSerialize(Stream<Map<String, String>> suggestions) {
         // get("label") returns a String ; get("value") returns a Map (the entry itself)
         var groupedSuggestions =
-                suggestions.sorted(Comparator.comparing(suggestion -> suggestion.get("term")))
-                        .collect(groupingBy(suggestion -> suggestion.get("category"),
-                                mapping(suggestion -> ImmutableList.of(
-                                        "label", suggestion.get("term"),
-                                        "options", GsonProvider.GSON.toJson(suggestion)),
-                                        toList())));
-        return groupedSuggestions;
+                suggestions
+                        .sorted(Comparator.comparing(suggestion -> (suggestion.get("term"))))
+                        .collect(
+                                groupingBy(
+                                        suggestion -> suggestion.get("category"),
+                                        mapping(suggestion ->
+                                                        ImmutableMap.of(
+                                                                "label", suggestion.get("term"),
+                                                                "value", GsonProvider.GSON.toJson(suggestion)),
+                                                toList())));
+
+        JsonArray jsonArray = new JsonArray();
+
+        ANALYTICS_SUGGESTER_NAMES_FOR_METADATA_SEARCH.stream()
+                .filter(propertyName -> groupedSuggestions.keySet().contains(propertyName.name))
+                .forEach(propertyName ->
+                        jsonArray.add(
+                                GsonProvider.GSON.toJsonTree(
+                                        ImmutableMap.of(
+                                                "label", propertyName.label,
+                                                "options", groupedSuggestions.get(propertyName.name)))));
+
+        return jsonArray;
     }
 }
