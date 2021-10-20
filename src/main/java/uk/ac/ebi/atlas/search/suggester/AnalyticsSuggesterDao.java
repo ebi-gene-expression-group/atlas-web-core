8743eaf4f6a82444d39f37b4c05950d9067de643
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-10-20 16:24:27 +0100

    Remove blank space and staled imports

--- src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterDao.java
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterDao.java
@@ -2,10 +2,8 @@ package uk.ac.ebi.atlas.search.suggester;
 
 import org.apache.solr.client.solrj.response.Suggestion;
 import uk.ac.ebi.atlas.species.Species;
-
 import java.util.stream.Stream;
 
 public interface AnalyticsSuggesterDao {
-
     Stream<Suggestion> fetchMetaDataSuggestions(String query, int limit, Species... species);
 }
--- src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterService.java
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/AnalyticsSuggesterService.java
@@ -1,10 +1,7 @@
 package uk.ac.ebi.atlas.search.suggester;
 
-import org.springframework.stereotype.Service;
-
 import java.util.Map;
 import java.util.stream.Stream;
-
 
 public interface AnalyticsSuggesterService {
     Stream<Map<String, String>> fetchMetaDataSuggestions(String query, String... species);
