0caee733812c1ed254e4ef91a2c2ee370ceaca14
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-10-20 16:39:56 +0100

    Remove duplicate code and split functionality into small functions

--- src/main/java/uk/ac/ebi/atlas/search/suggester/SolrSuggestionReactSelectAdapter.java
+++ src/main/java/uk/ac/ebi/atlas/search/suggester/SolrSuggestionReactSelectAdapter.java
@@ -5,11 +5,10 @@ import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonArray;
 import uk.ac.ebi.atlas.solr.analytics.AnalyticsPropertyName;
 import uk.ac.ebi.atlas.utils.GsonProvider;
-
 import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
-
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.mapping;
 import static java.util.stream.Collectors.toList;
@@ -39,45 +38,27 @@ public class SolrSuggestionReactSelectAdapter {
                                                                 "value", GsonProvider.GSON.toJson(suggestion)),
                                                 toList())));
 
-        JsonArray jsonArray = new JsonArray();
+        return  groupedSuggestions.keySet().contains("metadata")? getAnalyticsSuggestionsForReact(groupedSuggestions):
+                getBioEntitySuggestionsForReact(groupedSuggestions);
+    }
 
+    private static JsonArray getBioEntitySuggestionsForReact(Map<String, List<ImmutableMap<String, String>>> groupedSuggestions) {
+        var jsonArray = new JsonArray();
         ID_PROPERTY_NAMES.stream()
                 .filter(propertyName -> groupedSuggestions.keySet().contains(propertyName.name))
-                .forEach(propertyName ->
-                    jsonArray.add(
-                            GsonProvider.GSON.toJsonTree(
-                                    ImmutableMap.of(
-                                            "label", propertyName.label,
-                                            "options", groupedSuggestions.get(propertyName.name)))));
-
+                .forEach(propertyName -> jsonArray.add(GsonProvider.GSON.toJsonTree(
+                        ImmutableMap.of("label", propertyName.label,
+                                "options", groupedSuggestions.get(propertyName.name)))));
         return jsonArray;
     }
 
-    public static JsonArray metaDataSerialize(Stream<Map<String, String>> suggestions) {
-        // get("label") returns a String ; get("value") returns a Map (the entry itself)
-        var groupedSuggestions =
-                suggestions
-                        .sorted(Comparator.comparing(suggestion -> (suggestion.get("term"))))
-                        .collect(
-                                groupingBy(
-                                        suggestion -> suggestion.get("category"),
-                                        mapping(suggestion ->
-                                                        ImmutableMap.of(
-                                                                "label", suggestion.get("term"),
-                                                                "value", GsonProvider.GSON.toJson(suggestion)),
-                                                toList())));
-
-        JsonArray jsonArray = new JsonArray();
-
+    private static JsonArray getAnalyticsSuggestionsForReact(Map<String, List<ImmutableMap<String, String>>> groupedSuggestions) {
+        var jsonArray = new JsonArray();
         ANALYTICS_SUGGESTER_NAMES_FOR_METADATA_SEARCH.stream()
                 .filter(propertyName -> groupedSuggestions.keySet().contains(propertyName.name))
-                .forEach(propertyName ->
-                        jsonArray.add(
-                                GsonProvider.GSON.toJsonTree(
-                                        ImmutableMap.of(
-                                                "label", propertyName.label,
-                                                "options", groupedSuggestions.get(propertyName.name)))));
-
+                .forEach(propertyName -> jsonArray.add(GsonProvider.GSON.toJsonTree(
+                        ImmutableMap.of("label", propertyName.label,
+                                    "options", groupedSuggestions.get(propertyName.name)))));
         return jsonArray;
     }
 }
