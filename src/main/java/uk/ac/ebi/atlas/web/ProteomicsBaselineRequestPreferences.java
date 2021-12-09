29978553180afffe7924bd1cc024ff031d840638
Upendra Kumbham <upendra.kumbham@gmail.com>
2021-12-09 09:56:10 +0000

    Improve approach in displaying corresponding dataunits based on experiment datasets

--- src/main/java/uk/ac/ebi/atlas/web/ProteomicsBaselineRequestPreferences.java
+++ src/main/java/uk/ac/ebi/atlas/web/ProteomicsBaselineRequestPreferences.java
@@ -5,7 +5,7 @@ import uk.ac.ebi.atlas.search.SemanticQuery;
 
 public class ProteomicsBaselineRequestPreferences extends BaselineRequestPreferences<ExpressionUnit.Absolute.Protein> {
     private static final double DEFAULT_CUTOFF = 0.0d;
-    private boolean isNewType=false;
+    private ExpressionUnit.Absolute.Protein unit = ExpressionUnit.Absolute.Protein.RA;
 
     @Override
     public double getDefaultCutoff() {
@@ -21,13 +21,10 @@ public class ProteomicsBaselineRequestPreferences extends BaselineRequestPrefere
 
     @Override
     public ExpressionUnit.Absolute.Protein getUnit() {
-      if(isNewType) {
-          return ExpressionUnit.Absolute.Protein.RA;
-      }
-        return ExpressionUnit.Absolute.Protein.PPB;
+        return unit;
     }
 
-    public void setUnit() {
-        isNewType = true;
+    public void setUnit(ExpressionUnit.Absolute.Protein unit) {
+        this.unit = unit;
     }
 }
