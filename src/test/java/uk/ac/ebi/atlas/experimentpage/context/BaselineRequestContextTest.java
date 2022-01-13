30e7e2acda2e89bb7dd9cf5d778e85e114de0a53
Upendra Kumbham <upendra.kumbham@gmail.com>
2022-01-13 23:14:57 +0000

    Randomise testcase

--- src/test/java/uk/ac/ebi/atlas/experimentpage/context/BaselineRequestContextTest.java
+++ src/test/java/uk/ac/ebi/atlas/experimentpage/context/BaselineRequestContextTest.java
@@ -4,7 +4,6 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Lists;
 import org.junit.Test;
-import uk.ac.ebi.atlas.model.ExpressionUnit;
 import uk.ac.ebi.atlas.model.experiment.ExperimentDesign;
 import uk.ac.ebi.atlas.model.experiment.ExperimentDisplayDefaults;
 import uk.ac.ebi.atlas.model.experiment.sdrf.Factor;
@@ -17,6 +16,8 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateBlankString;
+import static uk.ac.ebi.atlas.testutils.RandomDataTestUtils.generateRandomAssayGroup;
 
 public class BaselineRequestContextTest {
 
@@ -191,37 +192,36 @@ public class BaselineRequestContextTest {
         var defaultQueryFactorType = "defaultQueryFactorType";
         var otherType = "otherQueryFactorType";
 
-        var ag1 = AssayGroupFactory.create("g1", "run11");
-        var ag2 = AssayGroupFactory.create("g2", "run21");
-        var ag3 = AssayGroupFactory.create("g3", "run22");
-        var assayGroups = ImmutableList.of(ag1, ag2, ag3);
+        var assayGroups = ImmutableList.of(generateRandomAssayGroup(), generateRandomAssayGroup());
 
         var experimentDesign = mock(ExperimentDesign.class);
 
-        var factors1 = new FactorSet();
-        factors1.add(new Factor(defaultQueryFactorType, "name for g1"));
-        factors1.add(new Factor(otherType, ""));
+        experimentDesign.putFactor(
+            assayGroups.get(0).getFirstAssayId(), defaultQueryFactorType, "liver");
+        experimentDesign.putFactor(
+            assayGroups.get(0).getFirstAssayId(), otherType, generateBlankString());
 
-        when(experimentDesign.getFactors("run11")).thenReturn(factors1);
+        var factors1 = new FactorSet();
+        factors1.add(new Factor(defaultQueryFactorType, "liver"));
+        factors1.add(new Factor(otherType, generateBlankString()));
+
+        when(experimentDesign.getFactors(assayGroups.get(0).getFirstAssayId())).thenReturn(factors1);
+
+        experimentDesign.putFactor(
+            assayGroups.get(1).getFirstAssayId(), defaultQueryFactorType, "heart");
+        experimentDesign.putFactor(
+            assayGroups.get(1).getFirstAssayId(), otherType, "foo");
 
         var factors2 = new FactorSet();
-        factors2.add(new Factor(defaultQueryFactorType, "name for g2"));
-        factors2.add(new Factor(otherType, "other type value 2"));
+        factors2.add(new Factor(defaultQueryFactorType, "heart"));
+        factors2.add(new Factor(otherType, "foo"));
 
-        when(experimentDesign.getFactors("run21")).thenReturn(factors2);
+        when(experimentDesign.getFactors(assayGroups.get(1).getFirstAssayId())).thenReturn(factors2);
 
-        var factors3 = new FactorSet();
-        factors3.add(new Factor(defaultQueryFactorType, "name for g3"));
-        factors3.add(new Factor(otherType, ""));
-
-        when(experimentDesign.getFactors("run22")).thenReturn(factors3);
-
-        BaselineRequestContext<ExpressionUnit.Absolute.Rna> subject =
-            new BaselineRequestContext<>(BaselineRequestPreferencesTest.get(),
+        var subject = new BaselineRequestContext<>(BaselineRequestPreferencesTest.get(),
                 MockExperiment.createBaselineExperiment(experimentDesign, assayGroups));
 
-        assertThat(subject.displayNameForColumn(ag1), (is("name for g1")));
-        assertThat(subject.displayNameForColumn(ag2), (is("name for g2, other type value 2")));
-        assertThat(subject.displayNameForColumn(ag3), (is("name for g3")));
+        assertThat(subject.displayNameForColumn(assayGroups.get(0)), (is("liver")));
+        assertThat(subject.displayNameForColumn(assayGroups.get(1)), (is("heart, foo")));
     }
 }
