b9994621f0402c4b0cef9d7072084cddf4a64e58
Pablo Moreno <pablo.a.moreno@gmail.com>
2022-01-25 09:57:52 +0000

    Missing parenthesis... will probably require some manual casting

--- src/main/java/uk/ac/ebi/atlas/configuration/JdbcConfig.java
+++ src/main/java/uk/ac/ebi/atlas/configuration/JdbcConfig.java
@@ -24,7 +24,7 @@ public class JdbcConfig {
                       @Value("${jdbc.url}") String jdbcUrl,
                       @Value("${jdbc.username}") String jdbcUserName,
                       @Value("${jdbc.password}") String jdbcPassword,
-                      @Value("${jdbc.max_pool_size") Integer maxPoolSize
+                      @Value("${jdbc.max_pool_size}") Integer maxPoolSize
                       ) {
         hikariConfig = new HikariConfig();
         if (maxPoolSize==null)
