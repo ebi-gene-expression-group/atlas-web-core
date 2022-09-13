5444dfdd5a500672c7784fce82afd20e8567479f
lingyun1010 <zhaolingyun1010@gmail.com>
2022-09-13 08:59:26 +0100

    Sophisticate logic for experiment alternative view
    links

--- src/main/java/uk/ac/ebi/atlas/trader/factory/BaselineExperimentFactory.java
+++ src/main/java/uk/ac/ebi/atlas/trader/factory/BaselineExperimentFactory.java
@@ -2,6 +2,8 @@ package uk.ac.ebi.atlas.trader.factory;
 
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 import uk.ac.ebi.atlas.experimentimport.ExperimentDto;
 import uk.ac.ebi.atlas.experimentimport.idf.IdfParserOutput;
@@ -11,6 +13,7 @@ import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperimentConfiguration;
 import uk.ac.ebi.atlas.species.SpeciesFactory;
 import uk.ac.ebi.atlas.trader.ConfigurationTrader;
+import uk.ac.ebi.atlas.trader.ExperimentTrader;
 
 import java.util.Collection;
 
@@ -21,11 +24,15 @@ import static com.google.common.collect.ImmutableList.toImmutableList;
 public class BaselineExperimentFactory implements ExperimentFactory<BaselineExperiment> {
     private final ConfigurationTrader configurationTrader;
     private final SpeciesFactory speciesFactory;
+    private final ExperimentTrader experimentRepository;
 
+    @Autowired
     public BaselineExperimentFactory(ConfigurationTrader configurationTrader,
-                                     SpeciesFactory speciesFactory) {
+                                     SpeciesFactory speciesFactory,
+                                     @Lazy ExperimentTrader experimentRepository) {
         this.configurationTrader = configurationTrader;
         this.speciesFactory = speciesFactory;
+        this.experimentRepository = experimentRepository;
     }
 
     @Override
@@ -39,7 +46,7 @@ public class BaselineExperimentFactory implements ExperimentFactory<BaselineExpe
 
         var configuration = configurationTrader.getExperimentConfiguration(experimentDto.getExperimentAccession());
         var factorsConfig = configurationTrader.getBaselineFactorsConfiguration(experimentDto.getExperimentAccession());
-        var alternativeViews = extractAlternativeViews(factorsConfig);
+        var alternativeViews = extractAlternativeViews(experimentDto.getExperimentAccession(), factorsConfig);
 
         return new BaselineExperiment(
                 experimentDto.getExperimentType(),
@@ -70,16 +77,22 @@ public class BaselineExperimentFactory implements ExperimentFactory<BaselineExpe
     }
 
     private ImmutablePair<ImmutableList<String>, ImmutableList<String>>
-    extractAlternativeViews(BaselineExperimentConfiguration factorsConfig) {
+    extractAlternativeViews(String experimentAccession, BaselineExperimentConfiguration factorsConfig) {
         return ImmutablePair.of(
                 ImmutableList.copyOf(factorsConfig.getAlternativeViews()),
                 factorsConfig.getAlternativeViews().stream()
                     .map(altViewAccession ->
-                            "View by " +
-                                    configurationTrader.getBaselineFactorsConfiguration(altViewAccession)
-                                            .getDefaultQueryFactorType()
-                                            .toLowerCase()
-                                            .replace("_", " "))
+                            altViewAccession.substring(2,6) == experimentAccession.substring(2,6) ?
+                                        "View by " +
+                                        configurationTrader.getBaselineFactorsConfiguration(altViewAccession)
+                                                .getDefaultQueryFactorType()
+                                                .toLowerCase()
+                                                .replace("_", " ")
+                                        :
+                                        "Related " +
+                                                experimentRepository.getPublicExperiment(altViewAccession).getType() + " experiment"
+
+                  )
                     .collect(toImmutableList()));
     }
 }
