ba4e8843dac3ab83bf2511069d58ae9c68cd5607
upendrakumbham <upendra.kumbham@gmail.com>
2023-02-14 11:11:35 +0000

    feature/181713000-pick-the-default-plot-type-and-parameter-values-from-the-backend (#111)
    * Add a query to fetch max priority experiment accession
    
    * Removed this method to resolve conflicts as part of develop branch merging process, then re added after merging develop
    
    * Update SQL query to give one accession

--- src/main/java/uk/ac/ebi/atlas/testutils/JdbcUtils.java
+++ src/main/java/uk/ac/ebi/atlas/testutils/JdbcUtils.java
@@ -325,4 +325,15 @@ public class JdbcUtils {
                 String.class,
                 accession);
     }
+
+    public String fetchExperimentAccessionByMaxPriority(){
+        return jdbcTemplate.queryForObject(
+                "SELECT experiment_accession " +
+                        "FROM scxa_dimension_reduction " +
+                        "WHERE  priority = (SELECT max(priority) FROM scxa_dimension_reduction) " +
+                        "GROUP BY experiment_accession" +
+                        " ORDER BY RANDOM() LIMIT 1",
+                String.class);
+    }
+
 }
