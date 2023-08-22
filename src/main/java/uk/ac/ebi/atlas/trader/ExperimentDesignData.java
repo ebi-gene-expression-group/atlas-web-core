ea5dd279c91ce0d66fa7494a915035a8ac23f81a
lingyun1010 <zhaolingyun1010@gmail.com>
2023-08-22 15:16:19 +0100

    Remove getter methods from @value

--- src/main/java/uk/ac/ebi/atlas/trader/ExperimentDesignData.java
+++ src/main/java/uk/ac/ebi/atlas/trader/ExperimentDesignData.java
@@ -11,16 +11,4 @@ public final class ExperimentDesignData {
     private final Map<String, List<String>> characteristics;
     private final Map<String, List<String>> factorValues;
     private final Map<String, List<String>> arrayDesigns;
-
-    public Map<String, List<String>> getCharacteristics() {
-        return this.characteristics;
-    }
-
-    public Map<String, List<String>> getFactorValues() {
-        return this.factorValues;
-    }
-
-    public Map<String, List<String>> getArrayDesigns() {
-        return this.arrayDesigns;
-    }
 }
