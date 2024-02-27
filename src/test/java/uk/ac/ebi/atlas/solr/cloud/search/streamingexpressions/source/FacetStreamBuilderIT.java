0e1de6f8ffa4554f8f3896102d507424e274e61c
Karoly Erdos <karoly@ebi.ac.uk>
2024-02-27 10:12:39 +0000

    Revert "Remove large experiments from fixtures and unit tests (#132)"
    This reverts commit b42a70df263059d614db4b7227ef8f34658f3e5c.

--- src/test/java/uk/ac/ebi/atlas/solr/cloud/search/streamingexpressions/source/FacetStreamBuilderIT.java
+++ src/test/java/uk/ac/ebi/atlas/solr/cloud/search/streamingexpressions/source/FacetStreamBuilderIT.java
@@ -20,6 +20,9 @@ import uk.ac.ebi.atlas.solr.cloud.collections.BulkAnalyticsCollectionProxy;
 import uk.ac.ebi.atlas.solr.cloud.search.SolrQueryBuilder;
 
 import javax.inject.Inject;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
@@ -34,7 +37,7 @@ import static uk.ac.ebi.atlas.solr.cloud.collections.BulkAnalyticsCollectionProx
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = TestConfig.class)
 class FacetStreamBuilderIT {
-    private static final String E_MTAB_5214 = "E-MTAB-5214";
+    private static final String E_MTAB_2770 = "E-MTAB-2770";
 
     @Inject
     private SolrCloudCollectionProxyFactory collectionProxyFactory;
@@ -66,8 +69,8 @@ class FacetStreamBuilderIT {
                                      .sortByCountsAscending()
                                      .build())) {
 
-            var broadQueryResults = broadQueryStreamer.get().map(Tuple::getFields).collect(toList());
-            var narrowQueryResults = narrowQueryStreamer.get().map(Tuple::getFields).collect(toList());
+            var broadQueryResults = broadQueryStreamer.get().map(Tuple::getMap).collect(toList());
+            var narrowQueryResults = narrowQueryStreamer.get().map(Tuple::getMap).collect(toList());
 
             assertThat(broadQueryResults.size()).isGreaterThan(narrowQueryResults.size());
 
@@ -93,7 +96,7 @@ class FacetStreamBuilderIT {
                                      .withQuery(solrQueryBuilder.build())
                                      .sortByCountsAscending()
                                      .build())) {
-            assertThat(filteredStreamer1.get().map(Tuple::getFields))
+            assertThat(filteredStreamer1.get().map(Tuple::getMap))
                     .extracting(BIOENTITY_IDENTIFIER.name())
                     .containsExactlyInAnyOrder(
                             filteredStreamer2.get()
@@ -106,7 +109,7 @@ class FacetStreamBuilderIT {
     void includeAverage() {
         // We need to specify a baseline experiment, otherwise avg(abs(expression_level)) will be 0
         var solrQueryBuilder = new SolrQueryBuilder<BulkAnalyticsCollectionProxy>();
-        solrQueryBuilder.addFilterFieldByTerm(EXPERIMENT_ACCESSION, E_MTAB_5214);
+        solrQueryBuilder.addFilterFieldByTerm(EXPERIMENT_ACCESSION, E_MTAB_2770);
         try (var filteredByExperimentStreamer =
                      TupleStreamer.of(new FacetStreamBuilder<>(bulkAnalyticsCollectionProxy, BIOENTITY_IDENTIFIER)
                             .withQuery(solrQueryBuilder.build())
@@ -172,7 +175,7 @@ class FacetStreamBuilderIT {
     }
 
     private static Stream<Arguments> solrQueryBuildersProvider() {
-        var assayGroups = IntStream.range(1, 16).boxed().map(i -> "g" + i).collect(toSet());
+        var assayGroups = IntStream.range(1, 16).boxed().map(i -> "g" + i.toString()).collect(toSet());
 
         var hugeSolrQueryBuilder =
                 new SolrQueryBuilder<BulkAnalyticsCollectionProxy>()
@@ -196,7 +199,7 @@ class FacetStreamBuilderIT {
                         .addFilterFieldByTerm(ASSAY_GROUP_ID, assayGroups)
                         .addFilterFieldByRangeMin(EXPRESSION_LEVEL, 10.0)
                         .addFilterFieldByRangeMinMax(EXPRESSION_LEVEL, 300.0, 600.0)
-                        .addQueryFieldByTerm(EXPERIMENT_ACCESSION, E_MTAB_5214);
+                        .addQueryFieldByTerm(EXPERIMENT_ACCESSION, E_MTAB_2770);
 
         return Stream.of(
                 Arguments.of(hugeSolrQueryBuilder, bigSolrQueryBuilder),
