18e343bd82d54716d1ff079a3b43638690bdc3d9
lingyun zhao <zhaolingyun1010@gmail.com>
2024-04-24 10:19:00 +0100

    Show all secondaryAccession resourse for PRIDE exp
    eriments

--- src/main/java/uk/ac/ebi/atlas/experimentpage/link/LinkToPride.java
+++ src/main/java/uk/ac/ebi/atlas/experimentpage/link/LinkToPride.java
@@ -7,25 +7,27 @@ import uk.ac.ebi.atlas.model.experiment.baseline.BaselineExperiment;
 import java.text.MessageFormat;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.NoSuchElementException;
+import java.util.ArrayList;
 import java.util.function.Function;
-
-import static java.util.Collections.singleton;
+import java.util.stream.Collectors;
 
 @Component
 public class LinkToPride extends ExternallyAvailableContent.Supplier<BaselineExperiment> {
-    private static final Function<BaselineExperiment, String> formatLabel =
-            e -> MessageFormat.format("PRIDE Archive: project {0}", e.getSecondaryAccessions().stream().findAny().get());
-    private static final Function<BaselineExperiment, String> formatLink =
-            e -> MessageFormat.format("https://www.ebi.ac.uk/pride/archive/projects/{0}", e.getSecondaryAccessions().stream().findAny().get());
+    private static final Function<String, String> formatLabel =
+            secondaryAccession -> MessageFormat.format("PRIDE Archive: project {0}", secondaryAccession);
+    private static final Function<String, String> formatLink =
+            secondaryAccession -> MessageFormat.format("https://www.ebi.ac.uk/pride/archive/projects/{0}", secondaryAccession);
 
-    private static final Function<BaselineExperiment, ExternallyAvailableContent.Description> createIcon =
+    private static final Function<String, ExternallyAvailableContent.Description> createIcon =
             formatLabel.andThen(label -> ExternallyAvailableContent.Description.create("icon-pride", label));
 
     @Override
     public Collection<ExternallyAvailableContent> get(BaselineExperiment experiment) {
         return  experiment.getSecondaryAccessions().isEmpty() ? Collections.emptyList() :
-                singleton(new ExternallyAvailableContent(formatLink.apply(experiment), createIcon.apply(experiment)));
+                experiment.getSecondaryAccessions()
+                        .stream()
+                        .map(accession -> new ExternallyAvailableContent(formatLink.apply(accession), createIcon.apply(accession)))
+                        .collect(Collectors.toCollection(ArrayList::new));
     }
 
     @Override
