ab8efe3f92e3dd528f29aaa4c292afc79914afe3
lingyun zhao <zhaolingyun1010@gmail.com>
2024-06-21 10:19:53 +0100

    Correct IT test for all links classes

--- src/test/java/uk/ac/ebi/atlas/experimentpage/link/LinkToGeoIT.java
+++ src/test/java/uk/ac/ebi/atlas/experimentpage/link/LinkToGeoIT.java
@@ -8,66 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static uk.ac.ebi.atlas.model.download.ExternallyAvailableContent.ContentType.SUPPLEMENTARY_INFORMATION;
 
 class LinkToGeoIT {
-    @RepeatedIfExceptionsTest(repeats = 5)
-    void linkIfMicroarrayExperimentIsOnGeo() {
-        var microarrayExperiment =
-                new ExperimentBuilder.MicroarrayExperimentBuilder()
-                        .withSecondaryAccessions(ImmutableList.of("GSE150361"))
-                        .build();
-        var subject = new LinkToGeo.Microarray();
-
-       // We can’t use URI::getPath because the redirect prefix messes it up :/
-        assertThat(subject.get(microarrayExperiment))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.uri.toString().endsWith("GSE150361"))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.description.type().equals("icon-geo"))
-                .hasSize(1);
-    }
 
     @RepeatedIfExceptionsTest(repeats = 5)
-    void linksIfProteomicsBaselineExperimentIsOnGeo() {
-        var proteomicsBaselineExperiment =
-                new ExperimentBuilder.BaselineExperimentBuilder()
-                        .withSecondaryAccessions(ImmutableList.of("GSE150361", "GSE5656"))
-                        .build();
-        var subject = new LinkToGeo.ProteomicsBaseline();
-
-        // We can’t use URI::getPath because the redirect prefix messes it up :/
-        assertThat(subject.get(proteomicsBaselineExperiment))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.uri.toString().endsWith("GSE150361"))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.uri.toString().endsWith("GSE5656"))
-                .anyMatch(externallyAvailableContent -> externallyAvailableContent.description.type().equals("icon-geo"))
-                .hasSize(2);
-    }
-
-    @RepeatedIfExceptionsTest(repeats = 5)
-    void linksIfRnaSeqBaselineExperimentIsOnGeo() {
-        var RnaSeqBaselineExperiment =
-                new ExperimentBuilder.BaselineExperimentBuilder()
-                        .withSecondaryAccessions(ImmutableList.of("GSE150361", "GSE5656"))
-                        .build();
-        var subject = new LinkToGeo.RnaSeqBaseline();
-
-        // We can’t use URI::getPath because the redirect prefix messes it up :/
-        assertThat(subject.get(RnaSeqBaselineExperiment))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.uri.toString().endsWith("GSE150361"))
-                .anyMatch(externallyAvailableContent ->
-                        externallyAvailableContent.uri.toString().endsWith("GSE5656"))
-                .anyMatch(externallyAvailableContent -> externallyAvailableContent.description.type().equals("icon-geo"))
-                .hasSize(2);
-    }
-
-    @RepeatedIfExceptionsTest(repeats = 5)
-    void linksIfDifferentialExperimentIsOnGeo() {
+    void linksIfExperimentIsOnGeo() {
         var differentialExperiment =
                 new ExperimentBuilder.DifferentialExperimentBuilder()
                         .withSecondaryAccessions(ImmutableList.of("GSE150361", "GSE5454"))
                         .build();
-        var subject = new LinkToGeo.Differential();
+        var subject = new LinkToGeo();
 
         // We can’t use URI::getPath because the redirect prefix messes it up :/
         assertThat(subject.get(differentialExperiment))
@@ -81,10 +29,7 @@ class LinkToGeoIT {
 
     @RepeatedIfExceptionsTest(repeats = 5)
     void linksToGeoShowInSupplementaryInformationTab() {
-        assertThat(new LinkToGeo.RnaSeqBaseline().contentType())
-                .isEqualTo(new LinkToGeo.ProteomicsBaseline().contentType())
-                .isEqualTo(new LinkToGeo.Differential().contentType())
-                .isEqualTo(new LinkToGeo.Microarray().contentType())
+        assertThat(new LinkToGeo().contentType())
                 .isEqualTo(SUPPLEMENTARY_INFORMATION);
     }
 }
