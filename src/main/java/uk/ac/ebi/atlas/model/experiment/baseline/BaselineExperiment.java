ba60de1c38132d7a4ebdd4acef723989ce292dc4
Karoly Erdos <karoly@ebi.ac.uk>
2025-07-02 14:38:36 +0100

    Fix logging in the getFactors method for debugging
    Need to remove this later

--- src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExperiment.java
+++ src/main/java/uk/ac/ebi/atlas/model/experiment/baseline/BaselineExperiment.java
@@ -74,7 +74,7 @@ public class BaselineExperiment extends Experiment<AssayGroup> {
     @Nullable
     public FactorGroup getFactors(AssayGroup assayGroup) {
         LOGGER.info("AssayGroup: {}", assayGroup);
-        LOGGER.info("AssayId2Factor: {}", assayId2Factor);
+        LOGGER.info("AssayId2Factor is not null: {}", assayId2Factor != null);
         LOGGER.info("AssayId2Factor - Factor in given assayID ({}): {}", assayGroup.getFirstAssayId(), assayId2Factor.getOrDefault(assayGroup.getFirstAssayId(), null));
         return assayId2Factor.getOrDefault(assayGroup.getFirstAssayId(), null);
     }
